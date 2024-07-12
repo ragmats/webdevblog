744af13b5b870a4caec588b4cc20b093059878c3
Steven Coy <stevencoy@gmail.com>
2024-07-11 23:57:31 -0700

    Trying to load env variables in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// console.log("VITE_BASE_URL:", import.meta.env.VITE_BASE_URL);
+// Load environment variables explicitly
+const env = loadEnv(process.env.NODE_ENV, process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Prod base
-  base: "/webdevblog/",
-  // Dev base
-  // base: "/",
+  base: env.VITE_BASE_URL || "/",
 });
