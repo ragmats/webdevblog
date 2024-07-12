484e7d002a84b6ef11ec12f95fa84bd905a3e54f
Steven Coy <stevencoy@gmail.com>
2024-07-11 23:04:41 -0700

    Deploy with hardcoded base path.

--- src/main.jsx
+++ src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter(
       element: <NotFound />,
     },
   ],
-  { basename: import.meta.env.VITE_BASE_URL || "/" }
+  { basename: "/webdevblog/" }
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// console.log("VITE_BASE_URL:", import.meta.env.VITE_BASE_URL);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || "/",
+  base: "/webdevblog/",
 });
