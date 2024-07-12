31c6184134ca65050a11d3acddf178a8ba47855d
Steven Coy <stevencoy@gmail.com>
2024-07-11 23:20:06 -0700

    Testing env variable console log from Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 export default function Footer() {
   const date = new Date();
+  console.log("vite base url from footer", import.meta.env.VITE_BASE_URL);
   return (
     <div className="footer-container">
       <div className="footer-photo-container">
