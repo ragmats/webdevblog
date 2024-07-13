fa96591dbd7144ba28153aff9f3a8980cda01abe
Steven Coy <stevencoy@gmail.com>
2024-07-13 13:15:47 -0700

    Made navbar fixed and adjusted reset button position on mobile.

--- src/components/CubeHeader.jsx
+++ src/components/CubeHeader.jsx
@@ -1,25 +1,23 @@
 import { Link } from "react-router-dom";
 export default function CubeHeader() {
   return (
-    <>
-      <div className="title-container">
-        <div className="cube-container">
-          <div className="face face-top">
-            Steven Coy
-            <br />
-            Web Developer
-          </div>
-          <div className="face face-left">
-            Web Apps, Components, Games, Design, Front-End
-          </div>
-          <div className="face face-front">
-            JavaScript, HTML, CSS, React, Next.js, Python, Django, Flask, Git
-          </div>
-          <div className="face face-back"></div>
-          <div className="face face-right"></div>
-          <div className="face face-bottom"></div>
+    <div className="title-container">
+      <div className="cube-container">
+        <div className="face face-top">
+          Steven Coy
+          <br />
+          Web Developer
         </div>
+        <div className="face face-left">
+          Web Apps, Components, Games, Design, Front-End
+        </div>
+        <div className="face face-front">
+          JavaScript, HTML, CSS, React, Next.js, Python, Django, Flask, Git
+        </div>
+        <div className="face face-back"></div>
+        <div className="face face-right"></div>
+        <div className="face face-bottom"></div>
       </div>
-    </>
+    </div>
   );
 }
